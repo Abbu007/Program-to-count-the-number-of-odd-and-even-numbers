1a246b645bddb8daae4ddbb9c02f4b2e96bec1bd
Abbu007 <94054374+Abbu007@users.noreply.github.com>
2021-11-10 19:52:44 +0530

    Add files via upload

---
+++ Program to count Odd and Even Numbers.py
@@ -0,0 +1,44 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "db99b1d9",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "x=(1, 2, 3, 4, 5, 6, 7, 8, 9) \n",
+    "Number_of_Even=0\n",
+    "Number_of_Odd=0\n",
+    "for i in x:\n",
+    "    if i%2==0:\n",
+    "        Number_of_Even=Number_of_Even+1\n",
+    "    else:\n",
+    "        Number_of_Odd=Number_of_Odd+1\n",
+    "print(\"Number of even numbers:\",Number_of_Even)\n",
+    "print(\"Numbre of odd numbers:\",Number_of_Odd)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
